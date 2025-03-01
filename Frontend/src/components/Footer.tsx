f183b9abcc47fac7f5118f38ddf05e0c9c19448f
afroBalogun <148563288+afroBalogun@users.noreply.github.com>
2025-03-01 12:19:46 +0100

    debug images

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC<Props> = ({ isLocationValid }) => {
 
     return (
         <footer className={`${isLocationValid ? "" : "absolute bottom-0"} w-full flex justify-center p-2  transition-all duration-200 mt-16`}
-            style={{backgroundImage: "url(static/images/praise.jpg)"}}
+            style={{backgroundImage: "url(images/praise.jpg)"}}
         >
             <p className="text-xs text-white max-[500px]:text-[.6em]">
             &copy; {year} The Citadel Global Community Church. All Rights Reserved.
