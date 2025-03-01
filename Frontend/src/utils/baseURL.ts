dd742cae32338c2827fcf8b1ee4bee27db52cc47
afroBalogun <148563288+afroBalogun@users.noreply.github.com>
2025-03-01 21:11:14 +0100

    baseURL debug

--- Frontend/src/utils/baseURL.ts
+++ Frontend/src/utils/baseURL.ts
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     // return "http://localhost:5000"
-    return "https://citadel-missionary-form.vercel.app/"
+    return "https://citadel-missionary-form.vercel.app"
  }
  
  export default getBaseUrl
